2c55007c02239e76ae5fa18a78820cbc7b0e80bf
Zalron <33308793+Zalron@users.noreply.github.com>
2018-03-01 10:37:58 +1100

    Version 0.03.2
    Added grid snap

---
+++ Game Lab I Prototype/Assets/Scripts/SnapGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapGrid : MonoBehaviour
+{
+    public float grid = 0.5f;
+    float x = 0f, y = 0f;
+	void Start () // Use this for initialization
+    {
+		
+	}
+	void Update () // Update is called once per frame
+    {
+        if (grid > 0f)
+        {
+            float reciprocalGrid = 1f / grid;
+            x = Mathf.Round(transform.position.x * reciprocalGrid) / reciprocalGrid;
+            y = Mathf.Round(transform.position.y * reciprocalGrid) / reciprocalGrid;
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+	}
+}
